cb6af28f9c0dfb1a2bbf996838fe1829b2b5655f
Martin Podloucký <podlouckymartin@gmail.com>
2022-04-16 21:15:43 +0200

    Zobrazení hlasujicích pro jednotlivé možnosti

--- client/poll.js
+++ client/poll.js
@@ -1,12 +1,26 @@
-const Option = (option) => {
-  return `<div class="option">${option.text}</div>`;
+const Option = (option, maxVotes) => {
+  const ratio = option.voters.length / maxVotes;
+
+  const barWidth = Number.isNaN(ratio) ? '0.5rem' : `${ratio * 100}%`;
+
+  return `
+    <div class="option">
+      <div class="option__text">${option.text}</div>  
+      <div class="option__bar" style="width: ${barWidth};"></div>
+      <div class="option__voters">
+        ${option.voters.map((voter) => `<span class="voter">${voter}</span>`).join('')}
+      </div>
+    </div>
+  `;
 };
 
 const Poll = (poll) => {
+  const maxVotes = Math.max(...poll.options.map((option) => option.voters.length));
+
   return `
     <h1>${poll.question}</h1>
     <div class="options">
-      ${poll.options.map((option) => Option(option)).join('')}
+      ${poll.options.map((option) => Option(option, maxVotes)).join('')}
     </div>
   `;
 };
